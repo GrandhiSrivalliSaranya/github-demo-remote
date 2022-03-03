c7f24e6d2db005cfae18e6bb3751767172452583
saranya <srivalli_grandhi@persistent.com>
2022-03-03 10:23:12 +0530

    added fn headers

--- math.py
+++ math.py
@@ -1,13 +1,17 @@
 #add
+#add implementation
 def add(x,y):
     return x+y
 #subtract
+#subtract implementation
 def subtract(x,y):
     return x-y
 #multiply
+#multiply implementation
 def multiply(x,y):
     return x*y    #on bug789
 #divide
+#divide implementation
 def divide(x,y):
     if y==0:
         return dividebyzero
