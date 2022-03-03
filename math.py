2af91173447d401c0f507aa25313f7713e15c40d
GrandhiSrivalliSaranya <62934014+GrandhiSrivalliSaranya@users.noreply.github.com>
2022-03-03 10:19:38 +0530

    intial commit of maht.py

---
+++ math.py
@@ -0,0 +1,18 @@
+#add
+def add(x,y):
+    return x+y
+#subtract
+def subtract(x,y):
+    return x-y
+#multiply
+def multiply(x,y):
+    return x*y    #on bug789
+#divide
+def divide(x,y):
+    if y==0:
+        return dividebyzero
+    else:
+        return x/y
+#square implementation
+def square(x):
+    return x*x
